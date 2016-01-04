2c47efb0b50ba680ea5f574c754b3e21c762a4a0
Allan Espinosa <allan.espinosa@outlook.com>
2016-01-04 00:27:33 -0600

    Deploy flannel properly (sort of)

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,9 +1,27 @@
 include_recipe 'apt'
 
-docker_service 'default' do
-  version '1.9.1'
+apt_repository 'docker' do
+  uri 'http://proxy.dev:3142/apt.dockerproject.org/repo'
+  components %w(debian-jessie main)
+  keyserver 'p80.pool.sks-keyservers.net'
+  key '58118E89F3A912897C070ADBF76221572C52609D'
+  cache_rebuild true
 end
 
-kube_master 'default' do
-  action :create
+etcd_service 'default' do
+  source 'http://proxy.dev:3142/github.com/coreos/etcd/releases/download/v2.2.3/etcd-v2.2.3-linux-amd64.tar.gz '
+  version '2.2.3'
+  service_manager 'systemd'
+  action %w(create start)
+end
+
+flannel_service 'default' do
+  action %w(create start)
+end.extend FlannelCookbook::SubnetParser
+
+docker_service 'default' do
+  bip lazy { resources('flannel_service[default]').subnetfile_subnet }
+  mtu lazy { resources('flannel_service[default]').subnetfile_mtu }
+  install_method 'package'
+  version '1.9.1'
 end
