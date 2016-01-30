6ebbf08703a5b998fd2e9cc9ef96e90a88f8b631
Allan Espinosa <allan.espinosa@outlook.com>
2016-01-30 16:42:51 -0600

    Wire the rest of kubelet flags to its unit

--- libraries/kubelet.rb
+++ libraries/kubelet.rb
@@ -54,13 +54,83 @@ module KubernetesCookbook
     end
 
     def kubelet_command
-      cmd = '/usr/sbin/kubelet'
-      if api_servers.is_a? Array
-        cmd << " --api-servers=#{api_servers.join ','}"
-      elsif api_servers.is_a? String
-        cmd << " --api-servers=#{api_servers}"
-      end
-      cmd
+      generator = CommandGenerator.new '/usr/sbin/kubelet', self
+      generator.generate
     end
+  end
+
+  # Commandline properties of a Kubelet
+  # Reference: http://kubernetes.io/v1.1/docs/admin/kubelet.html
+  class KubeletService < Chef::Resource
+    property :address, default: '0.0.0.0'
+    property :allow_privileged, default: false
+    property :api_servers
+    property :cadvisor_port, default: 4_194
+    property :cert_dir, default: '/var/run/kubernetes'
+    property :cgroup_root
+    property :chaos_chance, default: 0
+    property :cloud_config
+    property :cloud_provider
+    property :cluster_dns
+    property :cluster_domain
+    property :config
+    property :configure_cbr0, default: false
+    property :container_runtime, default: 'docker'
+    property :containerized, default: false
+    property :cpu_cfs_quota, default: false
+    property :docker_endpoint
+    property :docker_exec_handler, default: 'native'
+    property :enable_debugging_handlers, default: true
+    property :enable_server, default: true
+    property :event_burst, default: 0
+    property :event_qps, default: 0
+    property :file_check_frequency, default: '20s'
+    property :google_json_key
+    property :healthz_bind_address, default: '127.0.0.1'
+    property :healthz_port, default: 10_248
+    property :host_ipc_sources, default: '*'
+    property :host_network_sources, default: '*'
+    property :host_pid_sources, default: '*'
+    property :hostname_override
+    property :http_check_frequency, default: '20s'
+    property :image_gc_high_threshold, default: 90
+    property :image_gc_low_threshold, default: 80
+    property :kubeconfig, default: '/var/lib/kubelet/kubeconfig'
+    property :log_flush_frequency, default: '5s'
+    property :low_diskspace_threshold_mb, default: 256
+    property :manifest_url
+    property :manifest_url_header
+    property :master_service_namespace
+    property :max_open_files, default: 1_000_000
+    property :max_pods, default: 40
+    property :maximum_dead_containers, default: 100
+    property :maximum_dead_containers_per_container, default: 2
+    property :minimum_container_ttl_duration, default: '1m0s'
+    property :network_plugin
+    property :network_plugin_dir,
+             default: '/usr/libexec/kubernetes/kubelet_plugins/net/exec/'
+    property :node_status_update_frequency, default: '10s'
+    property :oom_score_adj, default: -999
+    property :pod_cidr
+    property :pod_infra_container_image,
+             default: 'gcr.io/google_containers/pause'
+    property :port, default: 10_250
+    property :read_only_port, default: 10_255
+    property :really_crash_for_testing, default: false
+    property :register_node, default: true
+    property :registry_burst, default: 10
+    property :registry_qps, default: 0
+    property :resolv_conf, default: '/etc/resolv.conf'
+    property :resource_container, default: '/kubelet'
+    property :rkt_path
+    property :rkt_stage1_image
+    property :root_dir, default: '/var/lib/kubelet'
+    property :runonce, default: false
+    property :serialize_image_pulls, default: true
+    property :streaming_connection_idle_timeout, default: 0
+    property :sync_frequency, default: '10s'
+    property :system_container
+    property :tls_cert_file
+    property :tls_private_key_file
   end
 end
--- test/unit/kubelet_test.rb
+++ test/unit/kubelet_test.rb
@@ -5,26 +5,69 @@ require 'minitest/autorun'
 
 class KubeletTest < Minitest::Test
   def kubelet
-    @kubelet ||= KubernetesCookbook::KubeletService.new 'test'
+    @kubelet ||= KubernetesCookbook::KubeletService.new 'testing'
+  end
+
+  def test_has_a_name
+    assert_equal 'testing', kubelet.name
+  end
+
+  def test_default_action_is_create
+    assert_equal [:create], kubelet.action
   end
 
   def test_default_api_server_is_nil
     assert_nil kubelet.api_servers
   end
+end
 
-  def test_default_kubelet_command_is_just_the_binary
-    assert_equal '/usr/sbin/kubelet', kubelet.kubelet_command
+class KubeletActionStartTest < Minitest::Test
+  require_relative 'provider_helper'
+
+  def provider
+    @provider ||= begin
+      run = Cheffish::ChefRun.new
+      resource = run.compile_recipe do
+        kubelet_service 'testing'
+      end
+      provider = resource.provider_for_action(:start)
+      provider.extend ProviderInspection
+    end
+  end
+
+  def test_passes_scheduler_command_to_systemd
+    provider.action_start
+
+    unit = provider.inline_resources.find 'template[/etc/systemd/system'\
+        '/kubelet.service]'
+
+    command = unit.variables[:kubelet_command]
+    assert_equal '/usr/sbin/kubelet', command
   end
 
   def test_kubelet_command_with_apiserver_flags
-    kubelet.api_servers 'http://127.0.0.1:8080'
+    provider.new_resource.api_servers 'http://127.0.0.1:8080'
+    provider.action_start
+
+    unit = provider.inline_resources.find 'template[/etc/systemd/system'\
+        '/kubelet.service]'
+
+    command = unit.variables[:kubelet_command]
     assert_match %r{--api-servers=http://127.0.0.1:8080},
-                 kubelet.kubelet_command
+                 command
   end
 
   def test_multiple_apiservers
-    kubelet.api_servers %w(http://127.0.0.1:8080 https://10.0.0.1:6443)
+    provider.new_resource
+            .api_servers %w(http://127.0.0.1:8080 https://10.0.0.1:6443)
+    provider.action_start
+
+    unit = provider.inline_resources.find 'template[/etc/systemd/system'\
+        '/kubelet.service]'
+
+    command = unit.variables[:kubelet_command]
+
     assert_match %r{--api-servers=http://127.0.0.1:8080,https://10.0.0.1:6443},
-                 kubelet.kubelet_command
+                 command
   end
 end
