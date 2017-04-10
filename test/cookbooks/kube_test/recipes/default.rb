f02c843bbef4e29ad7835a450f49b9b149c29e21
Keyan Pishdadian <kpishdadian@gmail.com>
2017-04-10 13:54:52 -0400

    Update apiserver test to use hash for options

--- test/cookbooks/kube_test/recipes/default.rb
+++ test/cookbooks/kube_test/recipes/default.rb
@@ -18,10 +18,13 @@ etcd_service 'default' do
   action %w(create start)
 end # Needed by the kube_apiserver[default]
 
+options = {
+  :service_cluster_ip_range => '10.0.0.1/24',
+  :etcd_servers => 'http://127.0.0.1:4001',
+  :insecure_bind_address => '0.0.0.0' # for convenience
+}
 kube_apiserver 'default' do
-  service_cluster_ip_range '10.0.0.1/24'
-  etcd_servers 'http://127.0.0.1:4001'
-  insecure_bind_address '0.0.0.0' # for convenience
+  options options
   action %w(create start)
 end
 
