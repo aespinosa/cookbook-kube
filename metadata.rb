5357797995a5e832b7f59e08a8266b64f218e84c
Allan Espinosa <allan.espinosa@outlook.com>
2017-04-19 10:09:24 -0500

    Pass the license metric

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name 'kube'
 maintainer 'Allan Espinosa'
 maintainer_email 'allan.espinosa@outlook.com'
 description 'Custom resources to build a Kubernetes Cluster'
-license 'Apache v2.0'
+license 'apachev2'
 
 source_url 'https://github.com/aespinosa/cookbook-kube'
 issues_url 'https://github.com/aespinosa/cookbook-kube/issues'
