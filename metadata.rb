b58752277f815d61a4fd692341fd2a4434ca4b17
Allan Espinosa <allan.espinosa@outlook.com>
2017-04-28 20:27:23 -0500

    Add chef_version to pass foodcritic 10.0

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,8 @@ maintainer_email 'allan.espinosa@outlook.com'
 description 'Custom resources to build a Kubernetes Cluster'
 license 'Apache-2.0'
 
+chef_version '>= 12.5'
+
 source_url 'https://github.com/aespinosa/cookbook-kube'
 issues_url 'https://github.com/aespinosa/cookbook-kube/issues'
 
