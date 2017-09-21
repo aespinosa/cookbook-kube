611ebc5034f6decd5e6fa869a8039d76368c5980
Tim Smith <tsmith@chef.io>
2017-09-21 15:31:18 -0700

    Update chef requirement in the metadata to match the readme
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'allan.espinosa@outlook.com'
 description 'Custom resources to build a Kubernetes Cluster'
 license 'Apache-2.0'
 
-chef_version '>= 12.7'
+chef_version '>= 12.11'
 
 source_url 'https://github.com/aespinosa/cookbook-kube'
 issues_url 'https://github.com/aespinosa/cookbook-kube/issues'
